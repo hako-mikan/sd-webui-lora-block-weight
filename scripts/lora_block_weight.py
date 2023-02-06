9243e2fcef98f37932ef770c8fa630746bd64826
hako-mikan <122196982+hako-mikan@users.noreply.github.com>
2023-02-06 21:12:31 +0900

    Update lora_block_weight.py

--- scripts/lora_block_weight.py
+++ scripts/lora_block_weight.py
@@ -1,24 +1,22 @@
 import modules.scripts as scripts
 import gradio as gr
-
-from modules.processing import Processed
-from modules.shared import opts, cmd_opts, state
 from linecache import clearcache
-from math import gamma
-import gradio as gr
+import os.path
+import random
+from PIL import Image, ImageFont, ImageDraw
+from fonts.ttf import Roboto
+import modules.shared as shared
+from modules import devices, sd_models, images,extra_networks
+from modules.shared import opts, state
 import modules.ui
-import glob
 import os
 import re
 import torch
 import math
-import pprint
-import random
+from modules.processing import process_images, Processed
 
-from modules import shared, devices, sd_models,extra_networks
+lxyz = ""
 
-from modules import  sd_models,script_callbacks
-from modules.shared import opts
 
 LORABLOCKS=["encoder",
 "down_blocks_0_attentions_0",
@@ -38,7 +36,9 @@ LORABLOCKS=["encoder",
 "up_blocks_3_attentions_1",
 "up_blocks_3_attentions_2"]
 
+loopstopper = True
 
+ATYPES =["none","Block ID","values","seed","Original Weights"]
 
 class Script(modules.scripts.Script):   
     def title(self):
@@ -60,6 +60,8 @@ OUTD:1,0,0,0,0,0,0,0,1,1,1,1,0,0,0,0,0\n\
 OUTS:1,0,0,0,0,0,0,0,0,0,0,0,1,1,1,1,1\n\
 OUTALL:1,0,0,0,0,0,0,0,1,1,1,1,1,1,1,1,1\n"
 
+        runorigin = scripts.scripts_txt2img.run
+
         path_root = scripts.basedir()
         filepath = os.path.join(path_root,"scripts", "lbwpresets.txt")
         lbwpresets=""
@@ -78,13 +80,27 @@ OUTALL:1,0,0,0,0,0,0,0,1,1,1,1,1,1,1,1,1\n"
 
         with gr.Accordion("LoRA Block Weight",open = False):
             with gr.Row():
-                lbw_useblocks =  gr.Checkbox(value = True,label="Active",interactive =True)
-                reloadtext = gr.Button(elem_id="lora block weights", value="Reload Presets",variant='primary')
-                savetext = gr.Button(elem_id="lora block weights", value="Save Presets",variant='primary')
-                openeditor = gr.Button(elem_id="lora block weights", value="Open TextEditor",variant='primary')
-            bw_ratiotags= gr.TextArea(label="",lines=2,value=rasiostags,visible =True,interactive =True) 
+                with gr.Column(min_width = 50, scale=1):
+                    lbw_useblocks =  gr.Checkbox(value = True,label="Active",interactive =True,elem_id="lbw_active")
+                with gr.Column(scale=5):
+                    bw_ratiotags= gr.TextArea(label="",lines=2,value=rasiostags,visible =True,interactive =True,elem_id="lbw_ratios") 
+            with gr.Accordion("XYZ plot",open = False):
+                gr.HTML(value="<p>changeable blocks : BASE,IN01,IN02,IN04,IN05,IN07,IN08,M00,OUT03,OUT04,OUT05,OUT06,OUT07,OUT08,OUT09,OUT10,OUT11</p>")
+                xyzsetting = gr.Checkbox(value = False, label="Active     ",interactive =True,elem_id="lbw_active")
+                xtype = gr.Dropdown(label="X Types         ", choices=[x for x in ATYPES], value=ATYPES [2],interactive =True,elem_id="lbw_xtype")
+                xmen = gr.Textbox(label="X Values         ",lines=1,value="0,0.25,0.5,0.75,1",interactive =True,elem_id="lbw_xmen")
+                ytype = gr.Dropdown(label="Y Types         ", choices=[y for y in ATYPES], value=ATYPES [1],interactive =True,elem_id="lbw_ytype")    
+                ymen = gr.Textbox(label="Y Values         " ,lines=1,value="IN05-OUT05",interactive =True,elem_id="lbw_ymen")
+                ztype = gr.Dropdown(label="Z type         ", choices=[z for z in ATYPES], value=ATYPES[0],interactive =True,elem_id="lbw_ztype")    
+                zmen = gr.Textbox(label="Z values         ",lines=1,value="",interactive =True,elem_id="lbw_zmen")
+
             with gr.Accordion("Weights setting",open = True):
-                lbw_loraratios = gr.TextArea(label="",value=lbwpresets,visible =True,interactive  = True)      
+                with gr.Row():
+                    reloadtext = gr.Button(value="Reload Presets",variant='primary',elem_id="lbw_reload")
+                    reloadtags = gr.Button(value="Reload Tags",variant='primary',elem_id="lbw_reload")
+                    savetext = gr.Button(value="Save Presets",variant='primary',elem_id="lbw_savetext")
+                    openeditor = gr.Button(value="Open TextEditor",variant='primary',elem_id="lbw_openeditor")
+                lbw_loraratios = gr.TextArea(label="",value=lbwpresets,visible =True,interactive  = True,elem_id="lbw_ratiospreset")      
         
         import subprocess
         def openeditors():
@@ -97,52 +113,211 @@ OUTALL:1,0,0,0,0,0,0,0,1,1,1,1,1,1,1,1,1\n"
             except OSError as e:
                 pass
 
+        def tagdicter(presets):
+            presets=presets.splitlines()
+            wdict={}
+            for l in presets:
+                w=[]
+                if ":" in l :
+                    key = l.split(":",1)[0]
+                    w = l.split(":",1)[1]
+                if len([w for w in w.split(",")]) == 17:
+                    wdict[key.strip()]=w
+            return ",".join(list(wdict.keys()))
+
         def savepresets(text):
             with open(filepath,mode = 'w') as f:
                 f.write(text)
 
         reloadtext.click(fn=reloadpresets,inputs=[],outputs=[lbw_loraratios])
+        reloadtags.click(fn=tagdicter,inputs=[lbw_loraratios],outputs=[bw_ratiotags])
         savetext.click(fn=savepresets,inputs=[lbw_loraratios],outputs=[])
         openeditor.click(fn=openeditors,inputs=[],outputs=[])
 
-        return lbw_loraratios,lbw_useblocks
+        def urawaza(active):
+            if active :
+                for obj in scripts.scripts_txt2img.alwayson_scripts:
+                    if "lora_block_weight" in obj.filename:
+                        scripts.scripts_txt2img.selectable_scripts.append(obj)
+                        scripts.scripts_txt2img.titles.append("LoRA Block Weight")
+                scripts.scripts_txt2img.run = newrun
+            else:
+                scripts.scripts_txt2img.run = runorigin
 
-    def process(self, p, loraratios,useblocks):    
-    #def process_batch(self, p, loraratios,useblocks,**args):
+        xyzsetting.change(fn=urawaza,inputs=[xyzsetting])
+
+        return lbw_loraratios,lbw_useblocks,xyzsetting,xtype,xmen,ytype,ymen,ztype,zmen
+
+    def process(self, p, loraratios,useblocks,xyzsetting,xtype,xmen,ytype,ymen,ztype,zmen):
+        #print("self =",self,"p =",p,"presets =",loraratios,"useblocks =",useblocks,"xyzsettings =",xyzsetting,"xtype =",xtype,"xmen =",xmen,"ytype =",ytype,"ymen =",ymen,"ztype =",ztype,"zmen =",zmen)
+        
         if useblocks:
             loraratios=loraratios.splitlines()
             lratios={}
-            for i,l in enumerate(loraratios):
-                lratios[l.split(":",1)[0]]=l.split(":",1)[1]
-
-            _, extra_network_data = extra_networks.parse_prompts(p.all_prompts[0:1])
-            calledloras = extra_network_data["lora"]
-            lorans = []
-            lorars = []
-            for called in calledloras:
-                if len(called.items) <3:continue
-                if called.items[2] in lratios:
-                    lorans.append(called.items[0])
-                    wei = lratios[called.items[2]]
-                    multiple = called.items[1]
-                    ratios = [w for w in wei.split(",")]
-                    for i,r in enumerate(ratios):
-                        if r =="R":
-                            ratios[i] = round(random.random(),3)
-                        elif r == "U":
-                            ratios[i] = round(random.uniform(-0.5,1.5),3)
-                        else:
-                            ratios[i] = float(r)
-                    print(f"LoRA Block weight :{called.items[0]}: {ratios}")
-                    lorars.append(ratios)
-            if len(lorars) > 0: load_loras_blocks(lorans,lorars,multiple)
-            
+            for l in loraratios:
+                l0=l.split(":",1)[0]
+                lratios[l0.strip()]=l.split(":",1)[1]
+            if xyzsetting and "XYZ" in p.prompt:
+                lratios["XYZ"] = lxyz
+            loradealer(p,lratios)
         return
 
-    def postprocess(self, p, processed, *args):
-        import lora
-        lora.loaded_loras.clear()
+    def run(self,p,presets,useblocks,xyzsetting,xtype,xmen,ytype,ymen,ztype,zmen):
+        if xyzsetting:
+            import lora
+            loraratios=presets.splitlines()
+            lratios={}
+            for l in loraratios:
+                l0=l.split(":",1)[0]
+                lratios[l0.strip()]=l.split(":",1)[1]
 
+            calledloranames = ""
+
+            if "XYZ" in p.prompt:
+                base = lratios["XYZ"] if "XYZ" in lratios.keys() else "1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1"
+            else: return
+
+            #ATYPES =["none","Block ID","values","seed","Base Weights"]
+
+            def dicedealer(am):
+                for i,a in enumerate(am):
+                    if a =="-1": am[i] = str(random.randrange(4294967294))
+                print(f"the die was thrown : {am}")
+
+            #print(f"xs:{xmen},ys:{ymen},zs:{zmen}")
+
+            def adjuster(a,at):
+                a = [a.strip() for a in a.split(',')]
+                if "seed" in at:dicedealer(a)
+                return a
+            
+            xs = adjuster(xmen,xtype)
+            ys = adjuster(ymen,ytype)
+            zs = adjuster(zmen,ztype)
+
+            ids = alpha =seed = ""
+            p.batch_size = 1
+
+            print(f"xs:{xs},ys:{ys},zs:{zs}")
+
+            images = []
+
+            def weightsdealer(alpha,ids,base):
+                blockid=["BASE","IN01","IN02","IN04","IN05","IN07","IN08","M00","OUT03","OUT04","OUT05","OUT06","OUT07","OUT08","OUT09","OUT10","OUT11"]
+                #print(f"weights from : {base}")
+                ids = [z.strip() for z in ids.split(' ')]
+                weights_t = [w.strip() for w in base.split(',')]
+                if ids[0]!="NOT":
+                    flagger=[False]*17
+                    changer = True
+                else:
+                    flagger=[True]*17
+                    changer = False
+                for id in ids:
+                    if id =="NOT":continue
+                    if "-" in id:
+                        it = [it.strip() for it in id.split('-')]
+                        if  blockid.index(it[1]) > blockid.index(it[0]):
+                            flagger[blockid.index(it[0]):blockid.index(it[1])+1] = [changer]*(blockid.index(it[1])-blockid.index(it[0])+1)
+                        else:
+                            flagger[blockid.index(it[1]):blockid.index(it[0])+1] = [changer]*(blockid.index(it[0])-blockid.index(it[1])+1)
+                    else:
+                        flagger[blockid.index(id)] =changer    
+                for i,f in enumerate(flagger):
+                    if f:weights_t[i]=alpha
+                outext = ",".join(weights_t)
+                #print(f"weights changed: {outext}")
+                return outext
+
+            def xyzdealer(a,at):
+                nonlocal ids,alpha,p,base
+                if "ID" in at:return
+                if "values" in at:alpha = a
+                if "seed" in at:
+                    p.seed = int(a)
+                if "Weights" in at:base = lratios[a]
+
+            grids = []
+            images =[]
+
+            totalcount = len(xs)*len(ys)*len(zs)
+            shared.total_tqdm.updateTotal(totalcount)
+            xc = yc =zc = 0
+            state.job_count = totalcount 
+
+            for z in zs:
+                images = []
+                yc = 0
+                xyzdealer(z,ztype)
+                for y in ys:
+                    xc = 0
+                    xyzdealer(y,ytype)
+                    for x in xs:
+                        xyzdealer(x,xtype)
+                        if "ID" in xtype:
+                            if "values" in ytype:c_base = weightsdealer(y,x,base)
+                            if "values" in ztype:c_base = weightsdealer(z,x,base)
+                        if "ID" in ytype:
+                            if "values" in xtype:c_base = weightsdealer(x,y,base)
+                            if "values" in ztype:c_base = weightsdealer(z,y,base)
+                        if "ID" in ztype:
+                            if "values" in xtype:c_base = weightsdealer(x,z,base)
+                            if "values" in ytype:c_base = weightsdealer(y,z,base)
+
+                        print(f"X:{xtype}, {x},Y: {ytype},{y}, Z:{ztype},{z}, base:{c_base} ({len(xs)*len(ys)*zc + yc*len(xs) +xc +1}/{totalcount})")
+                        
+                        global lxyz
+                        lxyz = c_base
+
+                        lora.loaded_loras.clear()
+                        processed:Processed = process_images(p)
+                        images.append(processed.images[0])
+                        xc += 1
+                    yc += 1
+                zc += 1
+                origin = loranames(processed.all_prompts) + ", "+ znamer(ztype,z,base)
+                grids.append(smakegrid(images,xs,ys,origin,p))
+            processed.images= grids
+            return processed
+
+def znamer(at,a,base):
+    if "ID" in at:return f"Block : {a}"
+    if "values" in at:return f"value : {a}"
+    if "seed" in at:return f"seed : {a}"
+    if "Weights" in at:return f"original weights :\n {base}"
+    else: return ""
+
+def loranames(all_prompts):
+    _, extra_network_data = extra_networks.parse_prompts(all_prompts[0:1])
+    calledloras = extra_network_data["lora"]
+    names = ""
+    for called in calledloras:
+        if len(called.items) <3:continue
+        names += called.items[0] 
+    return names
+
+def loradealer(p,lratios):
+    _, extra_network_data = extra_networks.parse_prompts(p.all_prompts[0:1])
+    calledloras = extra_network_data["lora"]
+    lorans = []
+    lorars = []
+    for called in calledloras:
+        if len(called.items) <3:continue
+        if called.items[2] in lratios:
+            lorans.append(called.items[0])
+            wei = lratios[called.items[2]]
+            multiple = called.items[1]
+            ratios = [w for w in wei.split(",")]
+            for i,r in enumerate(ratios):
+                if r =="R":
+                    ratios[i] = round(random.random(),3)
+                elif r == "U":
+                    ratios[i] = round(random.uniform(-0.5,1.5),3)
+                else:
+                    ratios[i] = float(r)
+            print(f"LoRA Block weight :{called.items[0]}: {ratios}")
+            lorars.append(ratios)
+    if len(lorars) > 0: load_loras_blocks(lorans,lorars,multiple)
 
 re_digits = re.compile(r"\d+")
 re_unet_down_blocks = re.compile(r"lora_unet_down_blocks_(\d+)_attentions_(\d+)_(.+)")
@@ -263,3 +438,83 @@ def load_loras_blocks(names, lwei=None,multi=1.0):
 
         locallora.multiplier = multi
         lora.loaded_loras.append(locallora)
+
+def smakegrid(imgs,xs,ys,currentmodel,p):
+    ver_texts = [[images.GridAnnotation(y)] for y in ys]
+    hor_texts = [[images.GridAnnotation(x)] for x in xs]
+
+    w, h = imgs[0].size
+    grid = Image.new('RGB', size=(len(xs) * w, len(ys) * h), color='black')
+
+    for i, img in enumerate(imgs):
+        grid.paste(img, box=(i % len(xs) * w, i // len(xs) * h))
+
+    grid = images.draw_grid_annotations(grid,int(p.width), int(p.height), hor_texts, ver_texts)
+    grid = draw_origin(grid, currentmodel,w*len(xs),h*len(ys),w)
+    if opts.grid_save:
+        images.save_image(grid, opts.outdir_txt2img_grids, "xy_grid", extension=opts.grid_format, prompt=p.prompt, seed=p.seed, grid=True, p=p)
+
+    return grid
+
+def draw_origin(grid, text,width,height,width_one):
+    grid_d= Image.new("RGB", (grid.width,grid.height), "white")
+    grid_d.paste(grid,(0,0))
+    def get_font(fontsize):
+        try:
+            return ImageFont.truetype(opts.font or Roboto, fontsize)
+        except Exception:
+            return ImageFont.truetype(Roboto, fontsize)
+    d= ImageDraw.Draw(grid_d)
+    color_active = (0, 0, 0)
+    fontsize = (width+height)//25
+    fnt = get_font(fontsize)
+
+    if grid.width != width_one:
+        while d.multiline_textsize(text, font=fnt)[0] > width_one*0.75 and fontsize > 0:
+            fontsize -=1
+            fnt = get_font(fontsize)
+    d.multiline_text((0,0), text, font=fnt, fill=color_active,align="center")
+    return grid_d
+
+def newrun(p, *args):
+        script_index = args[0]
+
+        if args[0] ==0:
+            script = None
+            for obj in scripts.scripts_txt2img.alwayson_scripts:
+                if "lora_block_weight" in obj.filename:
+                    script = obj 
+                    script_args = args[script.args_from:script.args_to]
+        else:
+            script = scripts.scripts_txt2img.selectable_scripts[script_index]
+            
+            if script is None:
+                return None
+
+            script_args = args[script.args_from:script.args_to]
+        print(p)
+        processed = script.run(p, *script_args)
+
+        shared.total_tqdm.clear()
+
+        return processed
+
+
+# def run(self, p, *args):
+#         script_index = args[0]
+
+#         if script_index == 0:
+#             return None
+
+#         script = self.selectable_scripts[script_index-1]
+
+#         if script is None:
+#             return None
+
+#         script_args = args[script.args_from:script.args_to]
+#         processed = script.run(p, *script_args)
+
+#         shared.total_tqdm.clear()
+
+#         return processed
+   
